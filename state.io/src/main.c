0371b8e69e3b5d67eca348f21e33be491641fb7b
hamedsy <hamed007.saboor@gmail.com>
2022-02-03 20:33:13 +0330

    sending more than one soldier / typing soldier numbers on the center of cities / my team's cities are more than one

--- state.io/src/main.c
+++ state.io/src/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 #include <time.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL2_gfxPrimitives.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 
 
 typedef struct City {
@@ -14,6 +16,7 @@ typedef struct City {
 	int y2;
 	int theta;
 	int flag;
+	int soldiers_num;
 } City;
 
 typedef struct Coordination {
@@ -21,21 +24,25 @@ typedef struct Coordination {
 	double y;
 } Coordination;
 
-const int SCREEN_WIDTH = 1100;
-const int SCREEN_HEIGHT = 600;
+const int SCREEN_WIDTH = 1100 , SCREEN_HEIGHT = 600;
 const int FPS = 60;
-const int CENTER_R = 15;
+const int CENTER_R = 15 , SOLDIER_R = 3;
+const int SOLDIERS_MAX_NUM = 15;
+
+SDL_Color WHITE = { 0 , 0 , 0 , 255 };
 
 int mei , mej , enemyi , enemyj;
 int mouseOnMe = 0 , isSendingSoldiers = 0;
 int n;
 
+// char solNumStr[10];
+
 City cities[4][6];
-City myCity;
-City enemyCity;
+// City myCity;
+// City enemyCity;
 
 Coordination begin , dest;
-Coordination mouse , soldier;
+Coordination mouse , soldier[100];
 Coordination tmp = {0};
 
 // funcs prototypes
@@ -44,46 +51,79 @@ int initializingCities();
 void print2DCity( int a , int b , City arr[a][b] );
 void printMap( SDL_Renderer* rend , int n );
 void sendingSoldiers( SDL_Renderer *rend );
+void solNumIncreasing();
 
 
 int main() {
 
 	srand(time(NULL));
 
+	int frame = 1;
+
 	if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) < 0) {
 		printf("error : %s\n" , SDL_GetError());
 		return 0;
 	}
+	if(TTF_Init() == -1) {
+		printf("error : %s\n" , TTF_GetError());
+		return 0;
+	}
+
 	// window , renderer , surface , texture , event
-	SDL_Window *myWindow = SDL_CreateWindow( "My Window" , SDL_WINDOWPOS_CENTERED , SDL_WINDOWPOS_CENTERED , 
+	SDL_Window *myWindow = SDL_CreateWindow( "My State.io" , SDL_WINDOWPOS_CENTERED , SDL_WINDOWPOS_CENTERED , 
 	SCREEN_WIDTH , SCREEN_HEIGHT , SDL_WINDOW_OPENGL );
 	SDL_Renderer *myRenderer = SDL_CreateRenderer( myWindow , -1 , SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
-	SDL_Surface *mySurface = IMG_Load("images/back.jpg");
-	SDL_Texture *myTexture = SDL_CreateTextureFromSurface(myRenderer , mySurface);
+	SDL_Surface *imgSurface = IMG_Load("images/back.jpg");
+	SDL_Texture *imgTexture = SDL_CreateTextureFromSurface(myRenderer , imgSurface);
 	SDL_Event *myEvent;
-
-	// SDL_Rect a;
-	// a.x / a.y / a.h / a.w
-	
+	// text
 	n = initializingCities();
-	//print2DCity( 4 , 6 , cities );
-	//printf("center of my city is: ( %d , %d )\n" , (myCity.x1 + myCity.x2) / 2 , (myCity.y1 + myCity.y2) / 2 );
+
+	TTF_Font *solNumFont = TTF_OpenFont("fonts/LiberationSerif-Regular.ttf" , 15);
+	SDL_Surface *solNumSurface[4][6];
+	SDL_Texture *solNumTexture[4][6];
+	SDL_Rect textRect[4][6];
+	
+	for(int i = 0; i < 4; i++) {
+		for(int j = 0; j < n; j++) {
+			textRect[i][j].x = ((cities[i][j].x1 + cities[i][j].x2) / 2) - (CENTER_R / 2);
+			textRect[i][j].y = ((cities[i][j].y1 + cities[i][j].y2) / 2) - (CENTER_R / 2);
+			
+		}
+	}
+	
 
 	// game loop
 	while( 1 ) {
+
+		for(int i = 0; i < 4; i++) {
+			for(int j = 0; j < n; j++) {
+				char solNumStr[10] = {0};
+				sprintf( solNumStr , "%d" , cities[i][j].soldiers_num );
+				solNumSurface[i][j] = TTF_RenderText_Solid( solNumFont , solNumStr , WHITE );
+				solNumTexture[i][j] = SDL_CreateTextureFromSurface( myRenderer , solNumSurface[i][j] );
+				SDL_QueryTexture( solNumTexture[i][j] , NULL , NULL , &textRect[i][j].w , &textRect[i][j].h );
+				SDL_QueryTexture( solNumTexture[i][j] , NULL , NULL , &textRect[i][j].w , &textRect[i][j].h );
+				SDL_FreeSurface( solNumSurface[i][j] );
+			}
+		}
 		
 		SDL_SetRenderDrawColor( myRenderer , 0xff , 0xff , 0xff , 0xff );
 		SDL_RenderClear( myRenderer );
 
-		SDL_RenderCopy( myRenderer , myTexture , NULL , NULL );
-
+		SDL_RenderCopy( myRenderer , imgTexture , NULL , NULL );
 		printMap( myRenderer , n );
+		for(int i = 0; i < 4; i++) {
+			for(int j = 0; j < n; j++) {
+				SDL_RenderCopy( myRenderer , solNumTexture[i][j] , NULL , &textRect[i][j] );
+			}
+		}
 
 		// event handling
 		if( !eventHandling( myRenderer ) )
 			break;
 		if ( mouseOnMe ) {
-			lineColor( myRenderer , (myCity.x1 + myCity.x2) / 2 , (myCity.y1 + myCity.y2) / 2
+			lineColor( myRenderer , (cities[mei][mej].x1 + cities[mei][mej].x2) / 2 , (cities[mei][mej].y1 + cities[mei][mej].y2) / 2
 			, mouse.x , mouse.y , 0xff000000 );
 		}
 
@@ -94,14 +134,26 @@ int main() {
 		SDL_RenderPresent( myRenderer );
 		SDL_Delay( 1000 / FPS );
 		SDL_RenderClear( myRenderer );
+
+		frame++;
+		if(frame % 60 == 0) {
+			solNumIncreasing();
+			frame = 1;
+		} 
 		
 	}
 
 	
 	SDL_DestroyWindow( myWindow );
 	SDL_DestroyRenderer( myRenderer );
-    SDL_DestroyTexture( myTexture );
+    SDL_DestroyTexture( imgTexture );
+	for(int i = 0; i < 4; i++) {
+		for(int j = 0; j < n; j++) {
+			SDL_DestroyTexture( solNumTexture[i][j] );
+		}
+	}
 	SDL_Quit();
+	IMG_Quit();
 	
     return 0;
 }
@@ -121,9 +173,14 @@ int eventHandling( SDL_Renderer *rend ) {
 			return 0; // Quit
 
 		if( ev.type == SDL_MOUSEBUTTONUP ) {
-			soldier.x = (myCity.x1 + myCity.x2) / 2;
-			soldier.y = (myCity.y1 + myCity.y2) / 2;
-			begin = soldier;
+			for(int i = 0; i < cities[mei][mej].soldiers_num; i++) {
+				soldier[i].x = ((cities[mei][mej].x1 + cities[mei][mej].x2) / 2) + ((rand() % 50) - 25);
+				soldier[i].y = ((cities[mei][mej].y1 + cities[mei][mej].y2) / 2) + ((rand() % 50) - 25);
+			}
+
+			begin.x = (cities[mei][mej].x1 + cities[mei][mej].x2) / 2;
+			begin.y = (cities[mei][mej].y1 + cities[mei][mej].y2) / 2;
+
 			dest = mouse;
 			if(mouseOnMe) isSendingSoldiers = 1;
 			else isSendingSoldiers = 0;
@@ -133,10 +190,20 @@ int eventHandling( SDL_Renderer *rend ) {
 		if( ev.type == SDL_MOUSEBUTTONDOWN ) {
 			if(ev.button.button == SDL_BUTTON_LEFT ) {
 
-				if( abs( ev.button.x - ( (myCity.x1 + myCity.x2) / 2 ) ) < CENTER_R  &&  
-				abs( ev.button.y - ( (myCity.y1 + myCity.y2) / 2 ) ) < CENTER_R ) {
-					mouseOnMe = 1;
+				for(int i = 0; i < 4; i++) {
+					for(int j = 0; j < n; j++) {
+						if(cities[i][j].flag == 1) {
+							mei = i; mej = j;
+							if( ev.button.x >= cities[mei][mej].x1 && ev.button.x <= cities[mei][mej].x2  &&  
+							ev.button.y >= cities[mei][mej].y1 && ev.button.y <= cities[mei][mej].y2 ) {
+								mouseOnMe = 1;
+								break;
+							}
+						}
+					}
+					if( mouseOnMe ) break;
 				}
+						
 			}
 		}
 
@@ -146,24 +213,53 @@ int eventHandling( SDL_Renderer *rend ) {
 
 
 void sendingSoldiers( SDL_Renderer *rend ) {
-	int flag = 0;
+	int flag = 0 , i , j;
 	if(!flag)
-		for(int i = 0; i < 4; i++) 
-			for(int j = 0; j < n; j++) 
+		for(i = 0; i < 4; i++) {
+			for(j = 0; j < n; j++) {
 				if( dest.x <= cities[i][j].x2 && dest.x >= cities[i][j].x1 &&
-					dest.y <= cities[i][j].y2 && dest.y >= cities[i][j].y1 ) {
-						// dest.x = ( cities[i][j].x2 + cities[i][j].x1 ) / 2;
-						// dest.y = ( cities[i][j].y2 + cities[i][j].y1 ) / 2;
-						flag = 1;
-						break;
-					}
-	if(flag){	
-		filledCircleColor( rend , soldier.x , soldier.y , 10 , 0xff000000 );
-		soldier.x += 5 * (dest.x - begin.x) / ( sqrt ( ( (dest.x - begin.x) * (dest.x - begin.x) ) + ( (dest.y - begin.y) * (dest.y - begin.y) ) ) );
-		soldier.y += 5 * (dest.y - begin.y) / ( sqrt ( ( (dest.x - begin.x) * (dest.x - begin.x) ) + ( (dest.y - begin.y) * (dest.y - begin.y) ) ) );
-		// printf("x is :%d and y is: %d\n" , dest.x , dest.y);
-		if( abs(soldier.x - dest.x) <= 2 && abs(soldier.y - dest.y) <= 2 ) {
-			isSendingSoldiers = 0;
+				dest.y <= cities[i][j].y2 && dest.y >= cities[i][j].y1 ) {
+					dest.x = (cities[i][j].x1 + cities[i][j].x2) / 2;
+					dest.y = (cities[i][j].y1 + cities[i][j].y2) / 2;
+					// printf("%d %d\n" , i , j);
+					flag = 1;
+					break;
+				}
+			}
+			if(flag) break;
+		}
+
+	if(flag){
+		for(int k = 0; k < cities[mei][mej].soldiers_num; k++) {
+
+			filledCircleColor( rend , soldier[k].x , soldier[k].y , SOLDIER_R , 0xffffffff );
+			soldier[k].x += 4 * (dest.x - begin.x) / ( sqrt ( ( (dest.x - begin.x) * (dest.x - begin.x) ) + ( (dest.y - begin.y) * (dest.y - begin.y) ) ) );
+			soldier[k].y += 4 * (dest.y - begin.y) / ( sqrt ( ( (dest.x - begin.x) * (dest.x - begin.x) ) + ( (dest.y - begin.y) * (dest.y - begin.y) ) ) );
+			if( abs(soldier[k].x - dest.x) <= 10 && abs(soldier[k].y - dest.y) <= 10 ) {
+				isSendingSoldiers = 0;
+				cities[i][j].soldiers_num -= cities[mei][mej].soldiers_num;
+				cities[mei][mej].soldiers_num = 0;
+				if(cities[i][j].soldiers_num < 0) {
+					cities[i][j].soldiers_num = -cities[i][j].soldiers_num;
+					cities[i][j].flag = 1;
+				}
+			}
+
+		}
+	}
+}
+
+void solNumIncreasing() {
+	for(int i = 0; i < 4; i++) {
+		for(int j = 0; j < n; j++) {
+			if(cities[i][j].flag == 0) {
+				if(cities[i][j].soldiers_num <= SOLDIERS_MAX_NUM) {
+					cities[i][j].soldiers_num++;
+				}
+			}
+			else {
+				cities[i][j].soldiers_num++;
+			}
 		}
 	}
 }
@@ -204,6 +300,8 @@ int initializingCities() {
 			if(i != 0) cities[i][j + 1].y1 = cities[i - 1][j + 1].y2 + (rand() % 20) + 30;
 			else cities[i][j + 1].y1 = (rand() % 30) + 40;
 
+			cities[i][j].soldiers_num = 0;
+
 		}
 	}
 
@@ -215,10 +313,12 @@ int initializingCities() {
 	} while( (enemyi == mei && enemyj == mej) );
 
 	cities[mei][mej].flag = 1;
-	cities[enemyi][enemyj].flag = 2;
+	cities[enemyi][enemyj].flag = 1;
+	cities[mei][mej].soldiers_num += 10;
+	cities[enemyi][enemyj].soldiers_num += 10;
 
-	myCity = cities[mei][mej];
-	enemyCity = cities[enemyi][enemyj];
+	// myCity = cities[mei][mej];
+	// enemyCity = cities[enemyi][enemyj];
 
 	return n;
 }
@@ -228,14 +328,14 @@ void printMap( SDL_Renderer* rend , int n ) {
 	for(int i = 0; i < 4; i++) {
 		for(int j = 0; j < n; j++) {
 			
-			if( i == mei && j == mej ) {
+			if( cities[i][j].flag == 1 ) {
 				roundedBoxRGBA( rend , cities[i][j].x1 , cities[i][j].y1 , cities[i][j].x2 , cities[i][j].y2 , cities[i][j].theta ,
 				200 , 10 , 10 , 255 );
 				filledCircleRGBA( rend , (cities[i][j].x1 + cities[i][j].x2) / 2 , (cities[i][j].y1 + cities[i][j].y2) / 2 ,
 				CENTER_R , 150 , 10 , 10 , 255 );				
 			}
 
-			else if( i == enemyi && j == enemyj ) {
+			else if( cities[i][j].flag == 2 ) {
 				roundedBoxRGBA( rend , cities[i][j].x1 , cities[i][j].y1 , cities[i][j].x2 , cities[i][j].y2 , cities[i][j].theta ,
 				10 , 200 , 10 , 255 );
 				filledCircleRGBA( rend , (cities[i][j].x1 + cities[i][j].x2) / 2 , (cities[i][j].y1 + cities[i][j].y2) / 2 ,
